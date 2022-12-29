5616107177411192c7dca63697f21779098ffb8e
cameron-fitz <fitzgerald@mymail.mines.edu>
2022-12-29 02:52:14 -0700

    started add process

--- rps.py
+++ rps.py
@@ -1,7 +1,21 @@
 # Open the file in read mode
 with open("input.txt", "r") as file:
-  # Use a for loop to read the lines of the file one by one
+	totSum = 0
 	for line in file:
-		print(line[0] + line[2])
-	
+		roundSum = 0
+		if line[2] == 'X':
+			#add 3 to round sum for choosing X
+			roundSum += 3
+			#add three if round end in draw
+			if = line[0] == 'X':
+				roundSum += 3	
+			#
+			elif line[0] == 'Z':
+				roundSum += 6	
+
+		elif line[2] == 'Y':
+			print(line[2] + ": " + "Paper" + "+ 2")
+		else:
+			print(line[2] + ": " + "Scissors" + "+ 1")
+
 	
