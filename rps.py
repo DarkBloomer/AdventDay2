141b3bd662302379322bf5a70ee3fce7ebd72c74
cameron-fitz <fitzgerald@mymail.mines.edu>
2022-12-30 10:27:05 -0700

    completed part 2

--- rps.py
+++ rps.py
@@ -6,39 +6,35 @@ with open("input.txt", "r") as file:
 		#keep track of round score
 		roundSum = 0
 
-		#handle Draw
-		if line[0] == 'A' and line[2] == 'X' or \
-		   line[0] == 'B' and line[2] == 'Y' or \
-		   line[0] == 'C' and line[2] == 'Z':  
-			roundSum += 3
 
-		#choose Rock
+		#choose Loss 
 		if line[2] == 'X':
-			#add 1 for Rock
-			roundSum += 1
-
-			#Rock beats Scissors
-			if line[0] == 'C':
-				roundSum += 6	
-
-		#choose Paper
-		elif line[2] == 'Y':
-			#add 2 for Paper
-			roundSum += 2
-
-			#Paper beats Rock	
 			if line[0] == 'A':
-				roundSum += 6
-				
-		#choose Scissors
-		else:
-			#add 3 for Scissors
-			roundSum += 3
+				roundSum += 3
+			elif line[0] == 'B':
+				roundSum += 1 
+			else:   
+				roundSum += 2
 
-			#Scissors beat Paper
-			if line[0] == 'B':
-				roundSum += 6
-		
+		#choose Draw 
+		elif line[2] == 'Y':
+			roundSum += 3
+			if line[0] == 'A':
+				roundSum += 1
+			elif line[0] == 'B':
+				roundSum += 2 
+			else:   
+				roundSum += 3
+				
+		#choose Win 
+		else:
+			roundSum += 6
+			if line[0] == 'A':
+				roundSum += 2
+			elif line[0] == 'B':
+				roundSum += 3 
+			else:   
+				roundSum += 1
 
 		totSum += roundSum	
 	print('Sum: ' + str(totSum))
