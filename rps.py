12f57b2f9b70fcf660aeae3203fa7574867a62ec
cameron-fitz <fitzgerald@mymail.mines.edu>
2022-12-30 09:56:42 -0700

    completed part 1 of Day 2

--- rps.py
+++ rps.py
@@ -2,20 +2,43 @@
 with open("input.txt", "r") as file:
 	totSum = 0
 	for line in file:
+					
+		#keep track of round score
 		roundSum = 0
-		if line[2] == 'X':
-			#add 3 to round sum for choosing X
+
+		#handle Draw
+		if line[0] == 'A' and line[2] == 'X' or \
+		   line[0] == 'B' and line[2] == 'Y' or \
+		   line[0] == 'C' and line[2] == 'Z':  
 			roundSum += 3
-			#add three if round end in draw
-			if = line[0] == 'X':
-				roundSum += 3	
-			#if player wins, add 6
-			elif line[0] == 'Z':
+
+		#choose Rock
+		if line[2] == 'X':
+			#add 1 for Rock
+			roundSum += 1
+
+			#Rock beats Scissors
+			if line[0] == 'C':
 				roundSum += 6	
 
+		#choose Paper
 		elif line[2] == 'Y':
-			print(line[2] + ": " + "Paper" + "+ 2")
-		else:
-			print(line[2] + ": " + "Scissors" + "+ 1")
+			#add 2 for Paper
+			roundSum += 2
 
-	
+			#Paper beats Rock	
+			if line[0] == 'A':
+				roundSum += 6
+				
+		#choose Scissors
+		else:
+			#add 3 for Scissors
+			roundSum += 3
+
+			#Scissors beat Paper
+			if line[0] == 'B':
+				roundSum += 6
+		
+
+		totSum += roundSum	
+	print('Sum: ' + str(totSum))
